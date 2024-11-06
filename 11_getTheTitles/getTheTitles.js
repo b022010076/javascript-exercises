13093991fe42eb831ad494da8d3d40e9aea4cea5
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-06 16:31:33 +0800

    Finish 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const result = [];
+    array.forEach((book) => {
+        result.push(book['title']);
+    })
+    return result;
 };
 
 // Do not edit below this line
