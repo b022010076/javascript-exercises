9b5e6db35577b72367a30f4b4caeb21858f4f2e1
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-06 14:14:44 +0800

    Finish 04_removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...element) {
+    array = array.filter(item => !(element.includes(item)));
+    return array;
 };
 
 // Do not edit below this line
