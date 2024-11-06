4ecaa24b8d65f0c453e40750713db269d7f12774
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-06 14:18:10 +0800

    Add solution method for 04_removeFromArray
    Use more newbie solution instead of more slightly advanced ones

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
 const removeFromArray = function(array, ...element) {
-    array = array.filter(item => !(element.includes(item)));
-    return array;
+    // array = array.filter(item => !(element.includes(item)));
+    // return array;
+
+    const newArray = [];
+    array.forEach((item) => {
+        if (!element.includes(item)) newArray.push(item);
+    })
+    return newArray;
 };
 
 // Do not edit below this line
