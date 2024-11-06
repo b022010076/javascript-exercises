0bbd466f237595e3c33d9dce216f8057e76d5abc
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-06 14:44:02 +0800

    Use solution method for 05_sumAll
    Easily use temp variable to store temporary values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -5,10 +5,17 @@ const sumAll = function(num1, num2) {
     if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
 
     let finalSum = 0;
-    let biggerNumber = num1 > num2 ? num1 : num2;
-    let smallerNumber = num2 < num1 ? num2 : num1;
+    // let biggerNumber = num1 > num2 ? num1 : num2;
+    // let smallerNumber = num2 < num1 ? num2 : num1;
+    if (num1 > num2) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
 
-    for (let i = smallerNumber; i<= biggerNumber; i++) {
+    // OR if (min > max) [min, max] = [max, min];
+
+    for (let i = num1; i<= num2; i++) {
         finalSum += i;
     }
 
