7679b052ed512e49b87cb356d136f44307b85d2e
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-06 16:47:33 +0800

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,6 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const currentTime = new Date();
+    return array.reduce((previous, current) => (previous.yearOfDeath ?? currentTime.getFullYear()) - previous.yearOfBirth > (current.yearOfDeath ?? currentTime.getFullYear()) - current.yearOfBirth ? previous : current);
 };
 
 // Do not edit below this line
