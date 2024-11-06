9a924a8ab0c68e157953a08613b688b3eb7a8dce
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-06 16:33:49 +0800

    Use solution method for getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,11 @@
 const getTheTitles = function(array) {
-    const result = [];
-    array.forEach((book) => {
-        result.push(book['title']);
-    })
-    return result;
+    // const result = [];
+    // array.forEach((book) => {
+    //     result.push(book['title']);
+    // })
+    // return result;
+
+    return array.map((book) => book.title);
 };
 
 // Do not edit below this line
