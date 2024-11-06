7756451efe0949d4d953dd4a5f5f1cb4720961ff
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-06 13:57:22 +0800

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let outputString;
 
+    const stringArray = string.split("");
+    const outputArray = [];
+
+    for (let i = stringArray.length - 1; i>=0; i--) {
+        outputArray.push(stringArray[i]);
+    }
+
+    outputString = outputArray.join("");
+    return outputString;
 };
 
 // Do not edit below this line
