affb9495076aa83b828b70e10a7f2f8f49bf5b49
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-06 15:01:17 +0800

    Finish 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // divisible by 4, % 4 = 0
+    // not divisible by 100, % 100 = 0
+    // unless divisible by 400, % 400 = 0
 
+    if (year % 4 == 0) {
+        if (year % 100 == 0 && year % 400 == 0) return true;
+        else if (year % 100 == 0) return false;
+        else return true;
+    } else return false;
 };
 
 // Do not edit below this line
