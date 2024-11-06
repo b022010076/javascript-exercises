09a01bc9c4b9c670f6efb216a97bba2e70befc3c
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-06 16:28:37 +0800

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(sequence) {
+    if (typeof sequence !== 'number') {
+        sequence = parseInt(sequence);
+    }
 
+    if (sequence < 0) return "OOPS";
+    if (sequence == 0) return 0;
+    
+    if (sequence == 1) {
+        return sequence;
+    } else return fibonacci(sequence - 1) + fibonacci(sequence - 2);
 };
 
 // Do not edit below this line
