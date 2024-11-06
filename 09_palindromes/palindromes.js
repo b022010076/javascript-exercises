5195746652c4f262969e3ee4bbbfb53f644f7ec4
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-06 16:05:30 +0800

    *Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // const filteredString = string.replace(/[^A-Za-z]/g, '').split("");
 
+    // return filteredString.join("").toLowerCase() == filteredString.reverse().join("").toLowerCase();
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const cleanedString = string
+        .toLowerCase()
+        .split("")
+        .filter((character) => alphanumerical.includes(character))
+        .join("");
+
+    const reversedString = cleanedString.split("").reverse().join("");
+
+    console.log(reversedString, cleanedString)
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
